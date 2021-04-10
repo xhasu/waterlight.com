d6a31006e79755e295f945b9f5f11d68e4b87a6d
xhasu <ing.xhasu@gmail.com>
2021-04-10 17:37:21 -0500

    responsive advance, add video youtube

--- components/sections/experience.jsx
+++ components/sections/experience.jsx
@@ -57,7 +57,25 @@ const Experience = () => {
       tlitem.from(item, {opacity: 0, y: '-15%'})
 
     })
-    
+
+    let tlprod = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.experience-armed .armed',
+        start: 'top center',
+        end:  'bottom center',
+        scrub: true,
+        markers: true,
+      }
+    })
+
+    tlprod.from('.armed img:nth-child(1)', {opacity: 0, y: '-15%'})
+      .from('.armed img:nth-child(2)', {opacity: 0, y: '-15%'})
+      .from('.armed img:nth-child(3)', {opacity: 0, y: '-15%'})
+      .from('.armed img:nth-child(4)', {opacity: 0, y: '-15%'})
+      .from('.armed img:nth-child(5)', {opacity: 0, y: '-15%'})
+      .from('.armed img:nth-child(6)', {opacity: 0, y: '-15%', duration: 2})
+      .from('.armed img:nth-child(7)', {opacity: 0, y: '-15%'})
+
     return () => { }
   }, [])
 
@@ -65,7 +83,7 @@ const Experience = () => {
     <section className="experience" id="experience">
       <div className="container">
 
-        <div className="panel compact experience-intro">
+        <div className="panel experience-intro">
           <div className="head-box expand">
             <div className="head-description">
               <p className="lead font-regular">{t('experience.subtitle')}</p>
@@ -74,42 +92,71 @@ const Experience = () => {
           </div>
         </div>
 
-        <div className="panel compact">
+        <div className="panel">
           <div className="head-box box-right compact">
+            <div className="experience-media">
+              <img src="/images/product/01.png" alt=""/>
+              <img src="/images/product/02.png" alt=""/>
+            </div>
             <div className="head-subtitle">{t('experience.stage1.title')}</div>
             <p>{t('experience.stage1.text')}</p>
           </div>
         </div>
 
-        <div className="panel compact">
+        <div className="panel">
           <div className="head-box box-right compact">
+            <div className="experience-media">
+              <img src="/images/product/03.png" alt=""/>
+            </div>
             <div className="head-subtitle">{t('experience.stage2.title')}</div>
             <p>{t('experience.stage2.text')}</p>
           </div>
         </div>
 
-        <div className="panel compact">
+        <div className="panel">
           <div className="head-box box-right compact">
+            <div className="experience-media">
+              <img src="/images/product/hook.png" alt=""/>
+            </div>
             <div className="head-subtitle">{t('experience.stage3.title')}</div>
             <p>{t('experience.stage3.text')}</p>
           </div>
         </div>
 
-        <div className="panel compact">
+        <div className="panel">
           <div className="head-box box-right compact">
+            <div className="experience-media">
+              <img src="/images/product/06.png" alt=""/>
+            </div>
             <div className="head-subtitle">{t('experience.stage4.title')}</div>
             <p>{t('experience.stage4.text')}</p>
           </div>
         </div>
 
-        <div className="panel compact">
+        <div className="panel">
           <div className="head-box box-right compact">
+            <div className="experience-media">
+              <img src="/images/product/07.png" alt=""/>
+            </div>
             <div className="head-subtitle">{t('experience.stage5.title')}</div>
             <p>{t('experience.stage5.text')}</p>
           </div>
         </div>
 
+        <div className=" experience-armed">
+          <div className="armed">
+            <img src="/images/product/01.png" alt="" />
+            <img src="/images/product/02.png" alt="" />
+            <img src="/images/product/03.png" alt="" />
+            <img src="/images/product/04.png" alt="" />
+            <img src="/images/product/05.png" alt="" />
+            {/* <img src="/images/product/hook.png" alt="" /> */}
+            <img src="/images/product/06.png" alt="" />
+            <img src="/images/product/07.png" alt="" />
+          </div>
+        </div>
       </div>
+
     </section>
   )
 }
--- components/ui/video.jsx
+++ components/ui/video.jsx
@@ -4,11 +4,16 @@ const Video = () => {
 
   return (
     <div className="video">
+      <div className="embed16by9">
+        <iframe width="1080" height="608" src="https://www.youtube.com/embed/GwmMKTeWS3I?rel=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
       {/* <img src="https://images.unsplash.com/photo-1617004302666-b2b58ec33e5e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixlib=rb-1.2.1&q=80&w=1920" alt=""/> */}
+      {/* 
       <video preload="metadata" controls={true} controlsList="nodownload" playsInline="playsinline" poster="https://images.unsplash.com/photo-1617004302666-b2b58ec33e5e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixlib=rb-1.2.1&q=80&w=1920">
         <source src="/media/waterlight.mp4" type="video/mp4"/>
         <img src="https://images.unsplash.com/photo-1617004302666-b2b58ec33e5e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixlib=rb-1.2.1&q=80&w=1920" alt="waterlight poster" width="1920" height="1080" />
       </video>
+      */}
     </div>
   )
 }
