232abea6dec96037eb8e5efe84d52ca8ddc3b581
xhasu <ing.xhasu@gmail.com>
2021-04-13 19:31:51 -0500

    add gtm

--- pages/_document.js
+++ pages/_document.js
@@ -11,13 +11,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          
+          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-W5LR6TW');`}} />
 	        <link rel="icon" href="/favicon.ico?v=2.0.0" />
 
           <link rel="stylesheet" href="/css/fonts.css"/>
       
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5LR6TW" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
           <Main />
           <NextScript />
         </body>
