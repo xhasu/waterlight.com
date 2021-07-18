21eadbbf217090cedd6c628110e269ca9fdf5999
xhasu <ing.xhasu@gmail.com>
2021-07-18 15:04:19 +0000

    Deploying to gh-pages from @ xhasu/waterlight.com@862066fc3e467173f2ccd67374f504c69f190afc 🚀

---
+++ _next/static/chunks/pages/index-654b911e285a2fdd99e3.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[11],{H0SL:function(n,_,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t("cMU6")}])},cMU6:function(n,_,t){"use strict";t.r(_);var u=t("nKUr"),c=(t("q1tI"),t("CWuI"));_.default=function(){return Object(u.jsx)(c.a,{})}}},[["H0SL",0,1,2,3,4]]]);
